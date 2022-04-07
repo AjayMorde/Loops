343dfd03aec15287e87c97d494e6e3349b9dbc30
AjayMorde <103033106+AjayMorde@users.noreply.github.com>
2022-04-07 23:49:16 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,23 @@
+package welcomeToJava;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+//		number from one to five
+		for(num = 1; num<=5; num=num+1)
+		{
+			
+			System.out.println(num);
+		}
+			
+//		number from 500 to one
+		for(num = 500; num>=1; num = num-1)
+		{
+			 System.out.println(num);
+		}
+
+	}
+
+}
